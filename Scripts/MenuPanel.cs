3578226de9850988b762c03255a2ca3d4cf4f658
ashikurofuto <91377249+ashikurofuto@users.noreply.github.com>
2022-04-18 10:34:10 +0300

    Update MenuPanel.cs

--- Scripts/MenuPanel.cs
+++ Scripts/MenuPanel.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class MenuPanel : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _sceneObjects;
     public Action OnMenuOpened;
 
     private void OnEnable()
